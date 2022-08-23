864a46422ab718b784b9dd3847b555a2b240a062
Aske-7 <nelsonenrique89@hotmail.com>
2022-08-23 19:07:56 +0000

    my first commit

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+
+    # create a list called animal_list1
+    animal_list1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+    
+    # display animal_list1
+    print(animal_list1)
+    
+main()
+
